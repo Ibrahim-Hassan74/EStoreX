5d5f83a7c695f64b28cffaa39b91c827dc0fd6d8
Ibrahim-Hassan74 <ih3046376@gmail.com>
2025-05-02 03:18:24 +0300

    starting with view by adding login view based on tailwind css

---
+++ E-StoreX.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,30 @@
+﻿using EStoreX.Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EStoreX.Core.DTO
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Display(Name = "First Name")]
+        public string? PersonName { get; set; }
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [EmailAddress(ErrorMessage = "{0} should be in a proper email address format")]
+        [Remote(action: "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email is already use")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "{0} should contains numbers only")]
+        public string? Phone { get; set; }
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "{0} should be at least 7 characters")]
+        public string? Password { get; set; }
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string? ConfirmPassword { get; set; }
+        public UserTypeOptions UserType { get; set; } = UserTypeOptions.User;
+    }
+}
